eb0e497389d4a61329ce95694bbbe36d3ed507f0
Tbsc <Tbsc@users.noreply.github.com>
2016-03-29 18:10:07 +0300

    Guis are removed until I figure out how to make them work

--- src/main/java/tbsc/techy/machine/furnace/BlockPoweredFurnace.java
+++ src/main/java/tbsc/techy/machine/furnace/BlockPoweredFurnace.java
@@ -1,7 +1,11 @@
 package tbsc.techy.machine.furnace;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 import tbsc.techy.block.BlockBasePoweredMachine;
 
@@ -10,8 +14,10 @@ import tbsc.techy.block.BlockBasePoweredMachine;
  */
 public class BlockPoweredFurnace extends BlockBasePoweredMachine {
 
+    public static int tileInvSize;
+
     public BlockPoweredFurnace() {
-        super(Material.iron, "blockPoweredFurnace");
+        super(Material.iron, "blockPoweredFurnace", 1); // TODO Change size when needed
     }
 
     @Override
@@ -19,4 +25,12 @@ public class BlockPoweredFurnace extends BlockBasePoweredMachine {
         return new TilePoweredFurnace();
     }
 
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing side, float hitX, float hitY, float hitZ) {
+        if (!worldIn.isRemote) {
+            // playerIn.openGui(Techy.instance, Techy.POWERED_FURNACE_GUI_ID, worldIn, pos.getX(), pos.getY(), pos.getZ());
+            // TODO Uncomment when GUIs are fixed
+        }
+        return true;
+    }
 }
