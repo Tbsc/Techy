8c850a5ec6011d963242440e63ac213435095a2c
Tbsc <Tbsc@users.noreply.github.com>
2016-03-29 18:07:37 +0300

    Util method added

--- src/main/java/tbsc/techy/tile/TileBase.java
+++ src/main/java/tbsc/techy/tile/TileBase.java
@@ -1,10 +1,16 @@
 package tbsc.techy.tile;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 
 /**
  * Created by tbsc on 3/27/16.
  */
 public class TileBase extends TileEntity {
+
+    public boolean isUsuableByPlayer(EntityPlayer player) {
+        // Makes sure you are in range of 8 blocks from the block
+        return player.getPosition().distanceSq(pos.getX(), pos.getY(), pos.getZ()) <= 64;
+    }
 
 }
