1e78824c1c613e33da703e4effd3c3ccb90baaec
Tbsc <Tbsc@users.noreply.github.com>
2016-04-25 20:52:27 +0300

    Will be used in the new recipe class

---
+++ src/main/java/tbsc/techy/recipe/RecipeInput.java
@@ -0,0 +1,36 @@
+package tbsc.techy.recipe;
+
+import cofh.lib.util.helpers.ItemHelper;
+import net.minecraft.item.ItemStack;
+
+/**
+ * Basic "recipe input" class. It can be either a string (ore name) or an ItemStack (item).
+ *
+ * Created by tbsc on 4/25/16.
+ */
+public class RecipeInput {
+
+    public ItemStack input;
+
+    private RecipeInput(ItemStack input) {
+        this.input = input;
+    }
+
+    public boolean isInputEqual(RecipeInput other) {
+        return other != null && input.getItem() == other.input.getItem() && input.getMetadata() == other.input.getMetadata();
+    }
+
+    public static RecipeInput createOreDictInput(String oreName, int stackSize, int metadata) {
+        ItemStack oreStack = ItemHelper.getOre(oreName);
+        if (oreStack == null) return null;
+        oreStack.stackSize = stackSize;
+        oreStack.setItemDamage(metadata);
+        return new RecipeInput(oreStack);
+    }
+
+    public static RecipeInput createItemStackInput(ItemStack input) {
+        if (input == null) return null;
+        return new RecipeInput(input);
+    }
+
+}
