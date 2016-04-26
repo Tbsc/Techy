190adad18d1cd9b29c45f49994011142e500db00
Tbsc <Tbsc@users.noreply.github.com>
2016-04-26 22:38:00 +0300

    Wrench compatibility with CoFH API

--- src/main/java/tbsc/techy/api/ITechyWrench.java
+++ src/main/java/tbsc/techy/api/ITechyWrench.java
@@ -1,9 +1,13 @@
 package tbsc.techy.api;
 
+import cofh.api.item.IToolHammer;
+
 /**
- * Used to declare items as wrenches
+ * Used to declare items as wrenches.
+ * Supports the Ender IO wrench API using FML's optional API
  *
  * Created by tbsc on 4/26/16.
  */
-public interface ITechyWrench {
+public interface ITechyWrench extends IToolHammer {
+
 }
