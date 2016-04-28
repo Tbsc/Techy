0f57fcb3c66f65fca91b8821aaa40d7eb08b5e77
Tbsc <Tbsc@users.noreply.github.com>
2016-04-28 17:57:33 +0300

    Changed positions, added text and tooltip.

--- src/main/java/tbsc/techy/client/gui/element/TabSides.java
+++ src/main/java/tbsc/techy/client/gui/element/TabSides.java
@@ -5,6 +5,8 @@ import cofh.lib.gui.element.TabBase;
 import tbsc.techy.api.Sides;
 import tbsc.techy.tile.TileBase;
 
+import java.util.List;
+
 /**
  * This tab will add support for changing the configuration of the ISidedInventory interface.
  *
@@ -20,17 +22,25 @@ public class TabSides extends TabBase {
         this.setSize(width, height);
         this.setName("Configure Sides");
         this.tile = tile;
-        addElement(new ElementButtonOptionSide(gui, 19, height - 14 - 24, 8, 8, tile, Sides.UP, tile.getConfigurationForSide(Sides.UP)));
-        addElement(new ElementButtonOptionSide(gui, 19, height - 14 - 14, 8, 8, tile, Sides.FRONT, tile.getConfigurationForSide(Sides.FRONT)));
-        addElement(new ElementButtonOptionSide(gui, 29, height - 14 - 14, 8, 8, tile, Sides.RIGHT, tile.getConfigurationForSide(Sides.RIGHT)));
-        addElement(new ElementButtonOptionSide(gui, 9, height - 14 - 14, 8, 8, tile, Sides.LEFT, tile.getConfigurationForSide(Sides.LEFT)));
-        addElement(new ElementButtonOptionSide(gui, 19, height - 14 - 4, 8, 8, tile, Sides.DOWN, tile.getConfigurationForSide(Sides.DOWN)));
-        addElement(new ElementButtonOptionSide(gui, 29, height - 14 - 4, 8, 8, tile, Sides.BACK, tile.getConfigurationForSide(Sides.BACK)));
+        addElement(new ElementButtonOptionSide(gui, 19, height - 10 - 24, 8, 8, tile, Sides.UP, tile.getConfigurationForSide(Sides.UP)));
+        addElement(new ElementButtonOptionSide(gui, 19, height - 10 - 14, 8, 8, tile, Sides.FRONT, tile.getConfigurationForSide(Sides.FRONT)));
+        addElement(new ElementButtonOptionSide(gui, 29, height - 10 - 14, 8, 8, tile, Sides.RIGHT, tile.getConfigurationForSide(Sides.RIGHT)));
+        addElement(new ElementButtonOptionSide(gui, 9, height - 10 - 14, 8, 8, tile, Sides.LEFT, tile.getConfigurationForSide(Sides.LEFT)));
+        addElement(new ElementButtonOptionSide(gui, 19, height - 10 - 4, 8, 8, tile, Sides.DOWN, tile.getConfigurationForSide(Sides.DOWN)));
+        addElement(new ElementButtonOptionSide(gui, 29, height - 10 - 4, 8, 8, tile, Sides.BACK, tile.getConfigurationForSide(Sides.BACK)));
+    }
+
+    @Override
+    public void addTooltip(List<String> list) {
+        list.add("Configure Sides");
     }
 
     @Override
     protected void drawBackground() {
         super.drawBackground();
+        if (isFullyOpened()) {
+            gui.getFontRenderer().drawString("Sides", posX + 24 - getFontRenderer().getStringWidth("Sides") / 2, posY + 4, 0x404040);
+        }
     }
 
     @Override
