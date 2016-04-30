60f06978280e0f86c91447fd114df2ebeeb2d9b8
Tbsc <Tbsc@users.noreply.github.com>
2016-04-30 09:45:59 +0300

    API Changes to IOperator Adds a stopOperation method and a setShouldOperate method

--- src/main/java/tbsc/techy/api/IOperator.java
+++ src/main/java/tbsc/techy/api/IOperator.java
@@ -7,24 +7,40 @@ import net.minecraft.util.ITickable;
  * provides methods for that.
  * It extends {@link ITickable} as the operation needs to run every tick.
  *
- * if (canOperate()) {
+ * if (canOperate() && shouldOperate()) {
  *    doOperation();
  *    setOperationStatus(true);
  * } else {
  *    setOperationStatus(false);
  * }
  *
- * This snippet of code shows how operations should be done.
+ * This code shows how to use this interface, in the most basic way.
  *
  * Created by tbsc on 3/29/16.
  */
 public interface IOperator extends ITickable {
 
     /**
-     * This class *should* do {@code canOperate()} before working to make sure it will
+     * This class *should* do {@link #canOperate()} before working to make sure it will
      * work.
+     * In implementations, this method typically will remove a bit from the input and add the output
+     * to the output slot. It sometimes will also spawn experience.
+     * Energy consumption typically is done every tick through the {@link ITickable} update() method,
+     * however it is optional.
      */
     void doOperation();
+
+    /**
+     * In order to stop the current progress and operation of the machine, there needs to be
+     * a method for that,
+     * What this method should do is reset the progress, and change the operation status to false,
+     * so just make it stop working.
+     * Because of the way operations are built, 1 tick after you stop the operation it should start
+     * it again (if it can operate and should operate), as it should just stop the operation, not
+     * prevent it from operating. For that, there is the {@link #shouldOperate()} and the
+     * {@link #setShouldOperate(boolean)} methods.
+     */
+    void stopOperating();
 
     /**
      * Checks in the class if it can operate, and returns that.
@@ -40,8 +56,8 @@ public interface IOperator extends ITickable {
     boolean canOperate();
 
     /**
-     * This method is different from {@code canOperate()} in a few ways.
-     * 1. {@code canOperate()} *just* checks if it can. {@code shouldOperate()} checks
+     * This method is different from {@link #canOperate()} in a few ways.
+     * 1. {@link #canOperate()} *just* checks if it can. This methods should check
      *    if it should operate. This is useful in the case of redstone control, as
      *    technically it *can* run, but shouldn't.
      * 2. Adds more code flexibility.
@@ -61,6 +77,12 @@ public interface IOperator extends ITickable {
      * @return current operation total time
      */
     int getOperationTotalProgress();
+
+    /**
+     * Changes if the tile should operate.
+     * @param shouldOperate new status of operation
+     */
+    void setShouldOperate(boolean shouldOperate);
 
     /**
      * @param isRunning the new status
