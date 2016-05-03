58433ccc867689cd9f7154c46383682e7cfd05ee
Tbsc <Tbsc@users.noreply.github.com>
2016-05-03 12:37:00 +0300

    Time boosters added

--- src/main/java/tbsc/techy/init/ItemInit.java
+++ src/main/java/tbsc/techy/init/ItemInit.java
@@ -3,10 +3,7 @@ package tbsc.techy.init;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import tbsc.techy.item.ItemBase;
-import tbsc.techy.item.ItemBoosterBase;
-import tbsc.techy.item.ItemBoosterEnergy;
-import tbsc.techy.item.ItemWrench;
+import tbsc.techy.item.*;
 
 /**
  * Loads and contains all of Techy's items.
@@ -17,13 +14,16 @@ public class ItemInit {
 
     public static ItemBase itemWrench;
     public static ItemBoosterBase itemBoosterEnergy;
+    public static ItemBoosterBase itemBoosterTime;
 
     public static void init() {
         itemWrench = new ItemWrench();
         itemBoosterEnergy = new ItemBoosterEnergy();
+        itemBoosterTime = new ItemBoosterTime();
 
         GameRegistry.registerItem(itemWrench);
         GameRegistry.registerItem(itemBoosterEnergy);
+        GameRegistry.registerItem(itemBoosterTime);
     }
 
     /**
@@ -34,6 +34,7 @@ public class ItemInit {
     public static void initModels() {
         itemWrench.initModel();
         itemBoosterEnergy.initModel();
+        itemBoosterTime.initModel();
     }
 
 }
