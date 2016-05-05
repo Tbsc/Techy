0aed789bab3cbe2a44bb09f17d67959d35a9fef1
Tbsc <Tbsc@users.noreply.github.com>
2016-05-05 12:39:12 +0300

    Support for crusher IMC recipes is added, and IMC ore dictionary recipe support is added

--- src/main/java/tbsc/techy/recipe/IMCRecipeHandler.java
+++ src/main/java/tbsc/techy/recipe/IMCRecipeHandler.java
@@ -3,6 +3,7 @@ package tbsc.techy.recipe;
 import com.google.common.collect.ImmutableList;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagString;
 import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 import org.apache.logging.log4j.Level;
@@ -26,12 +27,34 @@ public class IMCRecipeHandler {
                 String isRecipe = messageNBT.getString("RecipeType");
                 if (isRecipe != null) {
                     if (isRecipe.equalsIgnoreCase("PoweredFurnace")) {
-                        ItemStack input = ItemStack.loadItemStackFromNBT(messageNBT.getCompoundTag("Input"));
+                        Object input = messageNBT.getTag("Input");
                         ItemStack output = ItemStack.loadItemStackFromNBT(messageNBT.getCompoundTag("Output"));
                         float experience = messageNBT.getFloat("Experience");
                         int energyUsage = messageNBT.getInteger("EnergyUsage");
-                        PoweredFurnaceRecipes.instance().addItemStackRecipe(input, output, experience, energyUsage);
-                        FMLLog.log(Level.DEBUG, "Powered furnace recipe with " + input.getDisplayName() + " input and " + output.getDisplayName() + " output. Energy usage of " + energyUsage);
+                        if (input instanceof NBTTagString) {
+                            PoweredFurnaceRecipes.instance().addOreDictionaryRecipe(((NBTTagString) input).getString(), output, experience, energyUsage);
+                            FMLLog.log(Level.DEBUG, "Powered furnace recipe with " + ((NBTTagString) input).getString() + " input ore name and " + output.getDisplayName() + " output. Energy usage of " + energyUsage);
+                        } else if (input instanceof NBTTagCompound) {
+                            ItemStack inputStack = ItemStack.loadItemStackFromNBT((NBTTagCompound) input);
+                            PoweredFurnaceRecipes.instance().addItemStackRecipe(inputStack, output, experience, energyUsage);
+                            FMLLog.log(Level.DEBUG, "Powered furnace recipe with " + inputStack.getDisplayName() + " input stack and " + output.getDisplayName() + " output. Energy usage of " + energyUsage);
+                        }
+                    }
+                    if (isRecipe.equalsIgnoreCase("Crusher")) {
+                        Object input = messageNBT.getTag("Input");
+                        ItemStack output = ItemStack.loadItemStackFromNBT(messageNBT.getCompoundTag("FirstOutput"));
+                        ItemStack output2 = ItemStack.loadItemStackFromNBT(messageNBT.getCompoundTag("SecondOutput"));
+                        int output2Chance = messageNBT.getInteger("SecondOutputChance");
+                        float experience = messageNBT.getFloat("Experience");
+                        int energyUsage = messageNBT.getInteger("EnergyUsage");
+                        if (input instanceof NBTTagString) {
+                            CrusherRecipes.instance().addOreDictionaryRecipe(((NBTTagString) input).getString(), output, output2, output2Chance, experience, energyUsage);
+                            FMLLog.log(Level.DEBUG, "Powered furnace recipe with " + ((NBTTagString) input).getString() + " input ore name and " + output.getDisplayName() + " output. Energy usage of " + energyUsage);
+                        } else if (input instanceof NBTTagCompound) {
+                            ItemStack inputStack = ItemStack.loadItemStackFromNBT((NBTTagCompound) input);
+                            CrusherRecipes.instance().addItemStackRecipe(inputStack, output, output2, output2Chance, experience, energyUsage);
+                            FMLLog.log(Level.DEBUG, "Powered furnace recipe with " + inputStack.getDisplayName() + " input stack, " + output.getDisplayName() + " first output and " + output2.getDisplayName() + " second output. Energy usage of " + energyUsage);
+                        }
                     }
                 }
             }
