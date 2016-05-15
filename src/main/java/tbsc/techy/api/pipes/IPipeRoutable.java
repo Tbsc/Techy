592cd8f65921ced0ef98cf3b74a0ac56f5e81161
Tbsc <danielben60@gmail.com>
2016-05-15 20:27:42 +0300

    Added an interface for "pipe routables": What they do is they "carry" an object through the pipes, and this object gets passed through the pipes, until it reaches its destination and then gets removed from the whole network. An implementation for energy pipes is also added, and all it does is store the value for the destination and origin blocks, and store the amount of energy that passes through the pipes. IPipeRoutable works in the sense that you need to check if you are at your destination, then do whatever. If you can NOT route the item at all through the pipes, then return it to the origin, and when it gets to the destination or when it returns to the origin block if it failed, it needs to know how much energy to insert, and for that there is the routed object.

---
+++ src/main/java/tbsc/techy/api/pipes/IPipeRoutable.java
@@ -0,0 +1,48 @@
+package tbsc.techy.api.pipes;
+
+import net.minecraft.util.BlockPos;
+
+/**
+ * Pipes will work so, for example, items transfer through the pipe, an instance of
+ * this interface will be passed from the previous pipe, and that way the pipe can
+ * determine where to send the item based on the given data.
+ *
+ * Created by tbsc on 5/14/2016.
+ */
+public interface IPipeRoutable<T> {
+
+    /**
+     * When something flows through pipes, it needs to know where to go,
+     * so this method returns the block the thing should transfer *into*,
+     * NOT the pipe it should get to. So if you are trying to route items
+     * into a chest, this needs to return the {@link BlockPos} of the
+     * chest, not of the pipe that's connected to the chest.
+     *
+     * Pipes themselves should implement and decide where should it go
+     * next, and that's why it returns the destination and not the next
+     * pipe it should go to.
+     * @return Destination of current object transfer
+     */
+    BlockPos getDestination();
+
+    /**
+     * When items go into the network, they enter it only if they can.
+     * If they somehow entered the network because they found a valid
+     * destination, and then that destination became unavailable, the
+     * item should return to the inventory it came from, and not just
+     * stay in the network or drop as an item on the ground.
+     * Note, that what I've explained above is mostly only applicable
+     * to item pipes, as energy and fluid pipes will (most likely) just
+     * keep the energy or fluid in the pipe itself.
+     * @return Block the object came from
+     */
+    BlockPos getOrigin();
+
+    /**
+     * Returns the object of the thing that is transferred through the
+     * pipe.
+     * @return object of what's routed through the pipe
+     */
+    T getRoutedObject();
+
+}
---
+++ src/main/java/tbsc/techy/api/pipes/impl/EnergyPipeRoutable.java
@@ -0,0 +1,48 @@
+package tbsc.techy.api.pipes.impl;
+
+import net.minecraft.util.BlockPos;
+import net.minecraftforge.fml.common.FMLLog;
+import tbsc.techy.api.pipes.IPipeRoutable;
+
+/**
+ * Implementation for {@link IPipeRoutable}, that is used to transfer
+ * energy.
+ *
+ * Created by tbsc on 5/14/2016.
+ */
+public class EnergyPipeRoutable implements IPipeRoutable {
+
+    protected BlockPos origin;
+    protected BlockPos destination;
+    protected int energyTransferred;
+
+    private EnergyPipeRoutable(BlockPos origin, BlockPos destination, int energyTransferred) {
+        this.origin = origin;
+        this.destination = destination;
+        this.energyTransferred = energyTransferred;
+    }
+
+    public static EnergyPipeRoutable create(BlockPos origin, BlockPos destination, int energyTransferred) {
+        if (origin == null || destination == null || energyTransferred <= 0) {
+            FMLLog.warning("Something attempted to create an invalid EnergyPipeRoutable instance! This is an error!");
+            return null;
+        }
+        return new EnergyPipeRoutable(origin, destination, energyTransferred);
+    }
+
+    @Override
+    public BlockPos getDestination() {
+        return origin;
+    }
+
+    @Override
+    public BlockPos getOrigin() {
+        return destination;
+    }
+
+    @Override
+    public Object getRoutedObject() {
+        return energyTransferred;
+    }
+
+}
