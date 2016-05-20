1df7771e2bc1ae1494603c828ec0a34b1ddb60f8
Tbsc <Tbsc@users.noreply.github.com>
2016-05-20 11:25:51 +0300

    Added position utility methods for finding type matching neighbors by location and class types

--- src/main/java/tbsc/techy/api/PositionUtil.java
+++ src/main/java/tbsc/techy/api/PositionUtil.java
@@ -1,6 +1,11 @@
 package tbsc.techy.api;
 
+import net.minecraft.block.Block;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.IBlockAccess;
+import org.apache.commons.lang3.ArrayUtils;
 
 /**
  * Utility class for checking position
@@ -17,6 +22,46 @@ public class PositionUtil {
      */
     public static boolean isNeighbor(BlockPos thisBlock, BlockPos neighbor) {
         return thisBlock.north() == neighbor || thisBlock.south() == neighbor || thisBlock.east() == neighbor || thisBlock.west() == neighbor || thisBlock.up() == neighbor || thisBlock.down() == neighbor;
+    }
+
+    /**
+     * This method will loop through the neighbors of the given location, and
+     * if they match the given class, then it returns a list with applicable
+     * neighbors
+     * @param applicableNeighborTypes Type(s) of applicable class
+     */
+    public static BlockPos[] getApplicableNeighbors(IBlockAccess world, BlockPos thisBlock, Class<?>... applicableNeighborTypes) {
+        BlockPos[] applicableNeighbors = new BlockPos[]{};
+        for (EnumFacing side : EnumFacing.VALUES) {
+            BlockPos neighbor = thisBlock.offset(side);
+            Block block = world.getBlockState(neighbor).getBlock();
+            for (Class<?> clazz : applicableNeighborTypes) {
+                if (block.getClass().isInstance(clazz)) {
+                    ArrayUtils.add(applicableNeighbors, neighbor);
+                }
+            }
+        }
+        return applicableNeighbors;
+    }
+
+    /**
+     * This method will loop through the neighbors of the given location, and
+     * if the tile entity (if there is one) match the given class, then it returns a list with applicable
+     * neighbors
+     * @param applicableNeighborTypes Type(s) of applicable class of TILE!
+     */
+    public static BlockPos[] getTileApplicableNeighbors(IBlockAccess world, BlockPos thisBlock, Class<?>... applicableNeighborTypes) {
+        BlockPos[] applicableNeighbors = new BlockPos[]{};
+        for (EnumFacing side : EnumFacing.VALUES) {
+            BlockPos neighbor = thisBlock.offset(side);
+            TileEntity block = world.getTileEntity(neighbor);
+            for (Class<?> clazz : applicableNeighborTypes) {
+                if (block.getClass().isInstance(clazz)) {
+                    ArrayUtils.add(applicableNeighbors, neighbor);
+                }
+            }
+        }
+        return applicableNeighbors;
     }
 
 }
