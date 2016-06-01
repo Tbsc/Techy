af6dc7e88594fe7984a5ea4992d77f33bd9b145b
Tbsc <Tbsc@users.noreply.github.com>
2016-06-01 17:29:45 +0300

    Added fromOrdinal method in Sides

--- src/main/java/tbsc/techy/api/Sides.java
+++ src/main/java/tbsc/techy/api/Sides.java
@@ -9,12 +9,22 @@ import net.minecraft.util.EnumFacing;
  */
 public enum Sides {
 
-    FRONT(2), BACK(3), DOWN(EnumFacing.DOWN.ordinal()), UP(EnumFacing.UP.ordinal()), LEFT(4), RIGHT(5);
+    FRONT(2), BACK(3), DOWN(EnumFacing.DOWN.ordinal()), UP(EnumFacing.UP.ordinal()), LEFT(4), RIGHT(5), UNKNOWN(9001);
 
     public int side;
 
     Sides(int side) {
         this.side = side;
+    }
+
+    public static Sides fromOrdinal(int ordinal) {
+        if (ordinal == FRONT.ordinal()) return FRONT;
+        if (ordinal == BACK.ordinal()) return BACK;
+        if (ordinal == DOWN.ordinal()) return DOWN;
+        if (ordinal == UP.ordinal()) return UP;
+        if (ordinal == LEFT.ordinal()) return LEFT;
+        if (ordinal == RIGHT.ordinal()) return RIGHT;
+        return UNKNOWN;
     }
 
 }
