ccf549471b1453075ee7fbb1eca29c2729b8f286
Tbsc <Tbsc@users.noreply.github.com>
2016-06-26 14:25:38 +0300

    Bump to version 1.1.0

--- src/main/java/tbsc/techy/Techy.java
+++ src/main/java/tbsc/techy/Techy.java
@@ -46,7 +46,7 @@ public class Techy {
      * Basic data
      */
     public static final String MODID = "Techy";
-    public static final String VERSION = "1.0.9";
+    public static final String VERSION = "1.1.0";
     public static final String DEPENDENCIES = "required-after:Forge@[" // (taken from AE2) require forge
             + net.minecraftforge.common.ForgeVersion.majorVersion + '.' // majorVersion
             + net.minecraftforge.common.ForgeVersion.minorVersion + '.' // minorVersion
