f62fed895bc94fdbc143aef4288065ff6d534a75
tbsc <danielben60@gmail.com>
2016-07-10 15:50:13 +0300

    Added CoFHLib's IDismantleable and made ITechyDismantleable extend it, for more compatibility

--- src/main/java/tbsc/techy/api/wrench/ITechyDismantleable.java
+++ src/main/java/tbsc/techy/api/wrench/ITechyDismantleable.java
@@ -1,8 +1,13 @@
 package tbsc.techy.api.wrench;
 
+import cofh.api.block.IDismantleable;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.ArrayList;
 
 /**
  * This interface should be implemented on anything that can be dismantled. Note that not only
@@ -10,7 +15,38 @@ import net.minecraft.world.World;
  *
  * Created by tbsc on 09/07/2016.
  */
-public interface ITechyDismantleable {
+public interface ITechyDismantleable extends IDismantleable {
+
+    /**
+     * Default implementation of CoFH's IDismantleable.
+     * Calls {@link #dismantle(EntityLivingBase, World, BlockPos, boolean)}.
+     * @param player The player
+     * @param world The world
+     * @param x X position
+     * @param y Y position
+     * @param z Z position
+     * @param returnDrops Should return drops to the player's inventory
+     * @return Block's drops
+     */
+    @Override
+    default ArrayList<ItemStack> dismantleBlock(EntityPlayer player, World world, int x, int y, int z, boolean returnDrops) {
+        return dismantle(player, world, new BlockPos(x, y, z), returnDrops).getDrops();
+    }
+
+    /**
+     * Default implementation of CoFH's IDismantleable.
+     * Calls {@link #isDismantleable(EntityLivingBase, World, BlockPos)}.
+     * @param player The player
+     * @param world The world
+     * @param x X position
+     * @param y Y position
+     * @param z Z position
+     * @return Can dismantle
+     */
+    @Override
+    default boolean canDismantle(EntityPlayer player, World world, int x, int y, int z) {
+        return isDismantleable(player, world, new BlockPos(x, y, z));
+    }
 
     /**
      * Checks if the target can be dismantled.
@@ -28,8 +64,27 @@ public interface ITechyDismantleable {
      * @param dismantler The entity dismantling
      * @param world The world instance
      * @param target The block to dismantle
-     * @return Is the dismantle successful
+     * @param returnDrops Should return drops to the player's inventory
+     * @return Is the dismantle successful, and what dropped
      */
-    Result dismantle(EntityLivingBase dismantler, World world, BlockPos target);
+    DismantleResult dismantle(EntityLivingBase dismantler, World world, BlockPos target, boolean returnDrops);
+
+    class DismantleResult {
+
+        private final ArrayList<ItemStack> drops;
+        private final Result result;
+
+        public DismantleResult(ArrayList<ItemStack> drops, Result result) {
+            this.drops = drops;
+            this.result = result;
+        }
+
+        public ArrayList<ItemStack> getDrops() { return drops; }
+
+        public Result getResult() {
+            return result;
+        }
+
+    }
 
 }
