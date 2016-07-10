b4d190a670edb19ef6752f7c6caee78f075e79c3
tbsc <danielben60@gmail.com>
2016-07-10 14:01:54 +0300

    Added support for CoFHLib's IToolHammer in ITechyWrench

--- src/main/java/tbsc/techy/api/wrench/ITechyWrench.java
+++ src/main/java/tbsc/techy/api/wrench/ITechyWrench.java
@@ -1,7 +1,9 @@
 package tbsc.techy.api.wrench;
 
+import cofh.api.item.IToolHammer;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 
 /**
@@ -12,16 +14,55 @@ import net.minecraft.util.math.BlockPos;
  *
  * Created by tbsc on 09/07/2016.
  */
-public interface ITechyWrench {
+public interface ITechyWrench extends IToolHammer {
+
+    /**
+     * Default implementation of the CoFH tool hammer API, for Techy wrenches.
+     * Just calls the Techy methods.
+     * @param item
+     *            The itemstack for the tool. Not required to match equipped item (e.g., multi-tools that contain other tools)
+     * @param user
+     *            The entity using the tool
+     * @param x
+     *            X location of the block/tile
+     * @param y
+     *            Y location of the block/tile
+     * @param z
+     *            Z location of the block/tile
+     * @return Is the tool usable
+     */
+    @Override
+    default boolean isUsable(ItemStack item, EntityLivingBase user, int x, int y, int z) {
+        return canDismantle(item, user, new BlockPos(x, y, z));
+    }
+
+    /**
+     * Default implementation of the CoFH tool hammer API, for Techy wrenches.
+     * @param item
+     *            The ItemStack for the tool. Not required to match equipped item (e.g., multi-tools that contain other tools)
+     * @param user
+     *            The entity using the tool
+     * @param x
+     *            X location of the block/tile
+     * @param y
+     *            Y location of the block/tile
+     * @param z
+     *            Z location of the block/tile
+     */
+    @Override
+    default void toolUsed(ItemStack item, EntityLivingBase user, int x, int y, int z) {
+        dismantle(item, user, new BlockPos(x, y, z));
+    }
 
     /**
      * Checks if the entity holding the wrench can rotate the target at the position given, using the item.
      * @param wrench The wrench {@link ItemStack}
      * @param entity The entity using the wrench
      * @param target The position the entity targets
+     * @param rotateTo Side to rotate the target to
      * @return Can the entity rotate the target using the item
      */
-    boolean canRotate(ItemStack wrench, EntityLivingBase entity, BlockPos target);
+    boolean canRotate(ItemStack wrench, EntityLivingBase entity, BlockPos target, EnumFacing rotateTo);
 
     /**
      * Rotates the target "using" the item wrench given.
@@ -30,9 +71,10 @@ public interface ITechyWrench {
      * @param wrench The wrench {@link ItemStack}
      * @param entity The entity using the wrench
      * @param target The position the entity targets
+     * @param rotateTo Side to rotate the target to
      * @return If the rotation succeeded
      */
-    Result rotate(ItemStack wrench, EntityLivingBase entity, BlockPos target);
+    Result rotate(ItemStack wrench, EntityLivingBase entity, BlockPos target, EnumFacing rotateTo);
 
     /**
      * Checks if the entity holding the wrench can dismantle the target at the position given, using the item.
