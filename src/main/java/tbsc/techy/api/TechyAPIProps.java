e67a91b46e8e5db336c89fea15f9b971526c79b7
tbsc <danielben60@gmail.com>
2016-07-10 15:50:47 +0300

    Added package-info.java file for the API and a TechyAPIProps class for any API properties

---
+++ src/main/java/tbsc/techy/api/TechyAPIProps.java
@@ -0,0 +1,16 @@
+package tbsc.techy.api;
+
+import tbsc.techy.common.Techy;
+
+/**
+ * API Class for properties, such as API version and API id.
+ *
+ * Created by tbsc on 10/07/2016.
+ */
+public class TechyAPIProps {
+
+    public static final String OWNER = Techy.MODID;
+    public static final String PROVIDES = OWNER + "API";
+    public static final String API_VER = "0.0.1";
+
+}
---
+++ src/main/java/tbsc/techy/api/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains all of the mod's API, for external and internal use.
+ */
+@API(owner = TechyAPIProps.OWNER, provides = TechyAPIProps.PROVIDES, apiVersion = TechyAPIProps.API_VER)
+package tbsc.techy.api;
+
+import net.minecraftforge.fml.common.API;
