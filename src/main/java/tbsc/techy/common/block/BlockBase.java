dc2ad4c1d9f04dde42f5e987264fb8ea21536509
tbsc <danielben60@gmail.com>
2016-07-11 15:19:53 +0300

    Added base block class with support for capabilities

---
+++ src/main/java/tbsc/techy/common/block/BlockBase.java
@@ -0,0 +1,42 @@
+package tbsc.techy.common.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import tbsc.techy.api.loader.IHasCustomModel;
+import tbsc.techy.api.loader.IHasItemBlock;
+import tbsc.techy.common.Techy;
+
+/**
+ * Base class for blocks. To allow for advanced functionality, use capabilities.
+ *
+ * Created by tbsc on 10/07/2016.
+ */
+public abstract class BlockBase extends Block implements ICapabilityProvider, IHasItemBlock, IHasCustomModel {
+
+    public BlockBase(String regName, Material materialIn) {
+        super(materialIn);
+        setRegistryName(regName);
+        setUnlocalizedName(Techy.MODID + ":" + regName);
+        setCreativeTab(Techy.TAB_TECHY);
+    }
+
+    // Rendering
+
+    @Override
+    public void loadCustomModel() {
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+
+    // Item block
+
+    @Override
+    public ItemBlock getItemBlock() {
+        return new ItemBlock(this);
+    }
+
+}
