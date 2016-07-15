0e1f529d61b9ca07a97856ac7329629743dd1085
tbsc <danielben60@gmail.com>
2016-07-15 23:04:27 +0300

    Added copyright disclaimer to most files

--- src/main/java/tbsc/techy/api/loader/InstanceLoader.java
+++ src/main/java/tbsc/techy/api/loader/InstanceLoader.java
@@ -1,18 +1,18 @@
 /*
  * Copyright © 2016 Tbsc
  *
- * Butter is free software: you can redistribute it and/or modify
+ * Techy is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
- * Butter is distributed in the hope that it will be useful,
+ * Techy is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Butter.  If not, see <http://www.gnu.org/licenses/>.
+ * License along with Techy.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package tbsc.techy.api.loader;
