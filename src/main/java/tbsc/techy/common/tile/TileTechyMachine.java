6cd9dbce8afaf615277f5e67ac2f3fcb2c7f1d15
Tbsc <Tbsc@users.noreply.github.com>
2016-08-29 21:07:18 +0300

    Implemented IOperator support in TileTechyMachine

--- src/main/java/tbsc/techy/common/tile/TileTechyMachine.java
+++ src/main/java/tbsc/techy/common/tile/TileTechyMachine.java
@@ -17,18 +17,46 @@
 
 package tbsc.techy.common.tile;
 
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
+import net.minecraftforge.common.capabilities.Capability;
+import tbsc.techy.api.capability.TechyCapabilities;
+import tbsc.techy.api.operation.IOperator;
+
 /**
  * Subclass of {@link TileTechyBase}, that adds more support for machine stuff,
  * such as processing and automation.
- * Most of those stuff need to be implemented manually, because capabilities
- * can't do those things.
+ * Use {@link tbsc.techy.api.operation.OperatorBuilder} to define function, and set
+ * the value of {@link #operator} to the generated instance.
  *
  * Created by tbsc on 13/07/2016.
  */
-public class TileTechyMachine extends TileTechyBase {
+public class TileTechyMachine extends TileTechyBase implements ITickable {
+
+    protected IOperator operator;
+
+    @Override
+    public void update() {
+        if (operator.updateOperation())
+            this.markDirty();
+    }
 
     public TileTechyMachine(int invSize) {
         super(invSize);
+    }
+
+    @Override
+    public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
+        if (operator != null && capability == TechyCapabilities.CAPABILITY_OPERATOR)
+            return true;
+        return super.hasCapability(capability, facing);
+    }
+
+    @Override
+    public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
+        if (operator != null && capability == TechyCapabilities.CAPABILITY_OPERATOR)
+            return (T) operator;
+        return super.getCapability(capability, facing);
     }
 
 }
